07be15dd1bf5ece62b284ea87594feb1ddf267dd
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-07-22 11:03:20 -0400

    Refactorización del entorno de pruebas móviles e implementación de tests de e-commerce
    Se eliminaron archivos antiguos de widgets del reporte Allure para limpiar la estructura del informe.
    
    Se actualizó la configuración de Appium para utilizar variables de entorno en la ruta del archivo APK.
    
    Se agregó la dependencia dotenv para la gestión de variables de entorno.
    
    Se creó una nueva suite de pruebas para funcionalidades de e-commerce, incluyendo login, búsqueda de productos, agregado al carrito y verificación del contenido del carrito.
    
    Se implementó el patrón Page Object Model para una mejor organización y mantenibilidad de los tests.

--- QA-FinalProject-AutoTesting/Pruebas-de-Mobile/configs/appium.config.js
+++ QA-FinalProject-AutoTesting/Pruebas-de-Mobile/configs/appium.config.js
@@ -1,7 +1,20 @@
 // configs/appium.config.js
+import dotenv from 'dotenv';
+dotenv.config();
+
+const rutaAPK = process.env.RUTA_APK;
+console.log("Ruta del APK:", rutaAPK); 
+
 export const capabilities = {
   platformName: "Android",
   "appium:automationName": "UiAutomator2",
   "appium:deviceName": "emulator-5554",
-  "appium:app": "C:/Estudio_Victor_Vivas/Desafio_Latam/Desafio_Latam_Testing_Pdf/Modulo_06/ECommerce.apk",
+  "appium:app": rutaAPK,
+};
+
+export const wdOpts = {
+  hostname: '127.0.0.1',
+  port: 4723,
+  loglevel: 'info',
+  capabilities
 };
